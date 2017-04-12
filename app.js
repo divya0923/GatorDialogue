7463ecc2794493d18992368a85e615c1e2694d73
Divya Mahendran <mdivyaa.23@gmail.com>
2017-04-12 19:24:46 -0400

    updated couchDB server url

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 
 // nano config 
 //var nano = require('nano')('http://127.0.0.1:5984/')//  ;
-var nano = require('nano')('https://couchdb-1cf0b7.smileupps.com');
+var nano = require('nano')('https://couchdb-c0e747.smileupps.com');
 var user = nano.db.use('_users');
 var gatorDialogue = nano.db.use("gatordialoguedb");
 
